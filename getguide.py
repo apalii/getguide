3263c024b2c43d430686c8a1a28753aab688339f
python <dezz.ua@gmail.com>
2014-09-27 12:48:21 +0300

    parameters enhancement

--- getguide.py
+++ getguide.py
@@ -53,13 +53,13 @@ def find_range(first,last,list=links):
  
 if __name__ == "__main__":
     if sys.argv.__len__() == 1 or sys.argv.__len__() > 4:
-        print "\nHow to use :\ngetguide.py  <int>     <int>      <str>"
+        print "\nHow to use :\ngetguide.py  <int>   <  <int>      <str>"
         print "getguide.py <from MR> <to MR> <grep pattern>"
         print "\nExamples :\n~$ python getguide.py 35\t - all possible guides for one release."
         print "~$ python getguide.py 35 bill\t - the same as ~$ python getguide.py 35 bill | grep -i bill"
         print "~$ python getguide.py 35 40\t - all possible guides for range of releases(from 35 to 40)"
         print "~$ python getguide.py 35 40 int\t - equivalent to ~$ python getguide.py 35 40 | grep -i int\n\nTry again!"
-        exit()
+        exit(0)
  
     elif sys.argv.__len__() == 2:
         url_grep()
@@ -67,16 +67,22 @@ if __name__ == "__main__":
                
     elif sys.argv.__len__() == 3:
         if sys.argv[2].isdigit() :
-            url_grep()
-            find_range(sys.argv[1], sys.argv[2])
+            if int(sys.argv[1]) > int(sys.argv[2]):
+                print '\nFirst parameter should be less than second parameter !\n', sys.argv[1], '<', sys.argv[2], 'really ???\n'
+                exit(0)
+            else :
+                url_grep()
+                find_range(sys.argv[1], sys.argv[2])
         else :
             url_grep()
             grep_i(sys.argv[2])
             find_one(sys.argv[1],links2)
  
     elif sys.argv.__len__() == 4:
+        if int(sys.argv[1]) > int(sys.argv[2]):
+            print '\nFirst parameter should be less than second parameter !\n', sys.argv[1], '<', sys.argv[2], 'really ???\n'
+            exit(0)
+        else:
             url_grep()
             grep_i(sys.argv[3])
             find_range(sys.argv[1], sys.argv[2],links2)
-
-

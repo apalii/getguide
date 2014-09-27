7e806d0438611bfce89bee901df283799d720943
python <dezz.ua@gmail.com>
2014-09-27 12:23:12 +0300

    grep -i added

--- getguide.py
+++ getguide.py
@@ -6,18 +6,24 @@ import sys
 
 '''
 *Not complete.
+version = 3
+ 
 This simple program is designed to find all available links on guides:
  
 http://portaone.com/resources/docs/PortaSIP/m-r-32/PortaSIP_Switching_Server_MR32.pdf
 http://portaone.com/resources/docs/PortaSIP/m-r-32/PortaSIP_Media_Server_MR32.pdf
  
-For single release or even for needed range.
+For one release or even for needed range.
+"grep -i <pattern>" - included since v3 !
 '''
+ 
 links = []
+links2 = []
 urls = ['http://portaone.com/support/documentation/old/', 'http://portaone.com/support/documentation/']
  
 def url_grep() :  
     '''This function will fill the list "links" with direct links on guides. '''
+    global links
     for page in urls :
         html = u.urlopen(page)
         for i in html:
@@ -26,25 +32,51 @@ def url_grep() :
                 x = re.findall('(\/resources\/docs\S+pdf)', i)
                 links.append(''.join(x))
  
-def link_grep_range(first,last):  
-    url_grep()
-    gen = ['MR' + str(i) for i in xrange(int(first[2:]), int(last[2:]) + 1)]
-    for pattern in gen:
-        for i in links :
-            if re.search(pattern,i):
-                print 'http://portaone.com' + i
- 
-def url_grep_single(mr) :
-    url_grep()
+def grep_i(guide):
+    ''' this is an analog of the grep -i'''
+    global links
     for i in links :
+        if re.search(guide, i, re.IGNORECASE):
+            links2.append(i)
+ 
+def find_one(mr,list=links) :
+    for i in list :
         if re.search(mr.upper(),i):
             print 'http://portaone.com' + i
  
+def find_range(first,last,list=links):  
+    gen = ['MR' + str(i) for i in xrange(int(first), int(last) + 1)]
+    for pattern in gen:
+        for i in list :
+            if re.search(pattern,i):
+                print 'http://portaone.com' + i
+ 
 if __name__ == "__main__":
-    if sys.argv.__len__() == 1 or sys.argv.__len__() > 3:
-        print "\nExamples :\n$ python guide.py mr35\n$ python guide.py mr35 mr40\n\nTry again!"
+    if sys.argv.__len__() == 1 or sys.argv.__len__() > 4:
+        print "\nHow to use :\ngetguide.py  <int>     <int>      <str>"
+        print "getguide.py <from MR> <to MR> <grep pattern>"
+        print "\nExamples :\n~$ python getguide.py 35\t - all possible guides for one release."
+        print "~$ python getguide.py 35 bill\t - the same as ~$ python getguide.py 35 bill | grep -i bill"
+        print "~$ python getguide.py 35 40\t - all possible guides for range of releases(from 35 to 40)"
+        print "~$ python getguide.py 35 40 int\t - equivalent to ~$ python getguide.py 35 40 | grep -i int\n\nTry again!"
         exit()
+ 
     elif sys.argv.__len__() == 2:
-        url_grep_single(sys.argv[1])
+        url_grep()
+        find_one(sys.argv[1])
+               
     elif sys.argv.__len__() == 3:
-        link_grep_range(sys.argv[1], sys.argv[2])
+        if sys.argv[2].isdigit() :
+            url_grep()
+            find_range(sys.argv[1], sys.argv[2])
+        else :
+            url_grep()
+            grep_i(sys.argv[2])
+            find_one(sys.argv[1],links2)
+ 
+    elif sys.argv.__len__() == 4:
+            url_grep()
+            grep_i(sys.argv[3])
+            find_range(sys.argv[1], sys.argv[2],links2)
+
+
